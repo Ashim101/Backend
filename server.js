5b8606131f885ea50d2d56766f26defd21c44301
ashim101 <ashimsapkota250@gmail.com>
2024-11-08 22:35:48 +0545

    config

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+import connectDb from './config/mongodb.js'
+import connectCloudinary from './config/cloudinary.js'
 
 const app = express()
 
 const port = process.env.port || 3000;
-
+connectDb();
 
 app.use(express.json());
 app.use(cors());
@@ -14,5 +16,8 @@ app.use(cors());
 app.get('/', (req, res) => {
     res.send("Api workng")
 })
+
+connectCloudinary()
+
 
 app.listen(port, () => console.log("listening on port ", port))
